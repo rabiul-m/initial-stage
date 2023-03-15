18dd33062f91d9f42ffce321277f057933ba5e59
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-03-15 13:34:24 +0530

    added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let helloTest = ('this is for test');
 let name = (' first time');
-const history =   helloTest + name;
+// const history =   helloTest + name;
+const history =   helloTest + name + "22222";
 console.log(history);
